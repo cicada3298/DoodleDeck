4ee0b1ce7140932216743ba811b6d11760d7f80f
Vaibhav Chaudhary <vaibhavchaudhary898@gmail.com>
2025-04-20 19:40:15 +0530

    added home page ui

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,6 +1,18 @@
 "use client";
+import SideBar from "@/components/home/sidebar";
+import Header from "@/components/home/header";
+import Banner from "@/components/home/banner";
+
 export default function Home() {
   return (
-    <div>Home Page</div>
+    <div className="flex min-h-screen bg-white">
+      <SideBar/>
+      <div className="flex-1 flex flex-col ml-[72px]">
+        <Header/>
+        <main className="flex-1 p-6 overflow-y-auto pt-20">
+          <Banner/>
+        </main>
+      </div>
+    </div>
   );
 }
